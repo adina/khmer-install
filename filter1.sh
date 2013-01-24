fbf2e0e0e8cb9dd8b53f31f2ae43f06517b73a0e
Adina Howe <adina@fyang1.eeob.iastate.edu>
2013-01-24 15:25:32 -0600

    fixed typos

--- filter1.sh
+++ filter1.sh
@@ -1,8 +1,7 @@
 FILE=$1
-FILE2=$2
 WORKING_DIR='/kb/dev_container/modules/docs/build'
-echo 'Running diginorm on:' $FILE $FILE2
+echo 'Running diginorm on:' $FILE 
 
-python $WORKING_DIR/khmer/scripts/normalize-by-median.py -k 20 -C 20 -N 4 -x 1e6 -s $FILE.pass1.kh -R $FILE.pass1.report $FILE $FILE2
+python $WORKING_DIR/khmer/scripts/normalize-by-median.py -k 20 -C 20 -N 4 -x 1e6 -s $FILE.pass1.kh -R $FILE.pass1.report $FILE 
 
-python $WORKING_DIR/khmer/sandbox/filter-below-abund.py $FILE.pass1.kh $FILE.keep $FILE2.keep
+python $WORKING_DIR/khmer/sandbox/filter-below-abund.py $FILE.pass1.kh $FILE.keep 
